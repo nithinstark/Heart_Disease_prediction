76e8fe23c0b8d9b577510bc97a553c7a491bfc31
Nithin_Stark <120547334+nithinstark@users.noreply.github.com>
2024-04-29 08:58:53 +0530

    Add files via upload

---
+++ heart/project/myapp/static/js/script.js
@@ -0,0 +1,58 @@
+
+const form = document.querySelector("form")
+const container = document.querySelector(".container")
+const resultpositive = document.getElementById("result-positive")
+const resultnegative = document.getElementById("result-negative")
+resultpositive.style.display = "none"
+resultnegative.style.display = "none"
+
+const errorMsg = document.querySelector('.msg')
+errorMsg.style.display = 'none'
+
+const spinner = document.querySelector('.loader-container')
+spinner.style.display = 'none'
+
+
+form.addEventListener('submit', async (e) => {
+    
+    spinner.style.display = ''
+    e.preventDefault()
+    const formdata = new FormData(form).entries()
+    let res = await fetch('http://127.0.0.1:8000/weight/', {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify(Object.fromEntries(formdata))
+    })
+    if (res.status == 500) {
+        spinner.style.display = 'none'
+        errorMsg.style.display = ''
+        setTimeout(() => {
+            errorMsg.style.display = 'none'
+        }, 5000);
+    }
+    else if(res.status == 400) {
+        spinner.style.display = 'none'
+        errorMsg.style.display = ''
+        setTimeout(() => {
+            errorMsg.style.display = 'none'
+        }, 5000);
+    }
+
+    spinner.style.display - ''
+
+    let json = await res.json()
+    console.log(json)
+    container.style.display = 'none'
+
+    if (json == 1) {
+        spinner.style.display = 'none'
+        resultpositive.style.display = ''
+    }
+    else if (json == 0) {
+        spinner.style.display = 'none'
+        resultnegative.style.display = ''
+    }
+
+})
